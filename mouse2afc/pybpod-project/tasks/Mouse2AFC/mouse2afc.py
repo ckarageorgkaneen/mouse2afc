c25d5fe79b15f53b58ce7a55ad9b14e0deebd927
Henry J. Flynn <hjf39@drexel.edu>
2023-07-10 17:15:34 +0300

    Update pybpod protocol

--- mouse2afc/pybpod-project/tasks/Mouse2AFC/mouse2afc.py
+++ mouse2afc/pybpod-project/tasks/Mouse2AFC/mouse2afc.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import os
 import sys
-sys.path.append('MOUSE2AFC_PATH')
 from pybpodapi.protocol import Bpod
 from mouse2afc import Mouse2AFC
 bpod = Bpod(emulator_mode=True)
